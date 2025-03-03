9510e8dc6e0a908dac956a364f129b1e48de2684
wangWking <842749351@qq.com>
2025-03-03 16:25:37 +0800

    优化B1运动规划效果

--- mercury_b1/mercury_b1/slider_control.py
+++ mercury_b1/mercury_b1/slider_control.py
@@ -6,13 +6,6 @@ import math
 import time
 import traceback
 
-
-# 限制频率的时间间隔
-UPDATE_INTERVAL = 0.1  # 10 Hz
-
-# 上一次更新的时间戳
-last_update_time = 0
-
 class Slider_Subscriber(Node):
     def __init__(self):
         super().__init__("control_slider")
@@ -28,18 +21,18 @@ class Slider_Subscriber(Node):
         self.l = Mercury("/dev/left_arm", 115200)
         # right arm
         self.r = Mercury("/dev/right_arm", 115200)
-        self.l.set_movement_type(0) # 速度融合2 很耗时
-        self.r.set_movement_type(0)
+        time.sleep(0.05)
+        self.l.set_movement_type(2) 
+        self.r.set_movement_type(2)
+        time.sleep(0.05)
+        self.l.set_vr_mode(1)
+        self.r.set_vr_mode(1)
+        time.sleep(0.05)
+        self.l.set_filter_len(3, 20)
+        self.r.set_filter_len(3, 20)
         time.sleep(0.05)
         
     def listener_callback(self, msg):
-        global last_update_time
-
-        current_time = time.time()
-        if current_time - last_update_time < UPDATE_INTERVAL:
-            return  # 如果间隔时间不足，跳过此次更新
-
-        last_update_time = current_time
 
         data_list = []
         for index, value in enumerate(msg.position):
@@ -53,15 +46,12 @@ class Slider_Subscriber(Node):
         middle_arm = data_list[-3:]
         
         print('left_angles: {}, right_angles: {}, middle_angles: {}'.format(left_arm, right_arm, middle_arm))
-        try:
-            self.l.send_angles(left_arm, 80, _async=True)
-            self.r.send_angles(right_arm, 80, _async=True)
-            self.r.send_angle(11, middle_arm[0], 80, _async=True)
-            self.r.send_angle(12, middle_arm[1], 80, _async=True)
-            self.r.send_angle(13, middle_arm[2], 80, _async=True)
-        except Exception as e:
-            e = traceback.format_exc()
-            print("Failed to send angles: {}".format(e))
+        self.l.send_angles(left_arm, 16, _async=True)
+        self.r.send_angles(right_arm, 16, _async=True)
+        self.r.send_angle(11, middle_arm[0], 16, _async=True)
+        self.r.send_angle(12, middle_arm[1], 16, _async=True)
+        self.r.send_angle(13, middle_arm[2], 16, _async=True)
+
 
 
 
